b7d4fca9f43b393e5fa0ae8e24a44ac865fc2aa3
Muhammad Sulaiman T <144138399+mhd-sulu-786@users.noreply.github.com>
2024-01-12 02:54:16 +0000

    #style update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,8 +10,8 @@ const Header = ({ auth, user }) => {
   const [picIndex, setPicIndex] = useState(0);
 
   const images = [
-    'https://tse4.mm.bing.net/th?id=OIP.tTO9bDuLi1PpUuT6OflBAgHaJ4&pid=Api&P=0&w=300&h=300',
     logo,
+    'https://tse4.mm.bing.net/th?id=OIP.tTO9bDuLi1PpUuT6OflBAgHaJ4&pid=Api&P=0&w=300&h=300',
     'https://tse1.mm.bing.net/th?id=OIP.8sEQq9-fsOY0T-R-vYtVqgHaIB&pid=Api&P=0&h=180',
     'https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg',
     'https://tse4.mm.bing.net/th?id=OIP.1Lww31q-RdrcMEVGs2mf5AHaKL&pid=Api&P=0&h=180'
