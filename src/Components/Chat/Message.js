184af3c04152692a05dd4e831ec3b1127e0ea7bd
Muhammad Sulaiman T <144138399+mhd-sulu-786@users.noreply.github.com>
2024-01-12 09:19:06 +0530

    Update Message.js

--- src/Components/Chat/Message.js
+++ src/Components/Chat/Message.js
@@ -6,7 +6,7 @@ const Message = ({message,user}) => {
    const messegeClass = uid===user.uid?"sent":"recive";
   return (
     <div className={`messege-shower ${messegeClass}`}>
-      <img src={messegeClass==="sent"?photourl:'https://tse3.mm.bing.net/th?id=OIP.cC-2qUAIWmvXAvxhUlnnLAHaH5&pid=Api&P=0&h=180'||profile} alt=''/>
+      <img src={messegeClass==="sent"?photourl:'https://tse3.mm.bing.net/th?id=OIP.cC-2qUAIWmvXAvxhUlnnLAHaH5&pid=Api&P=0&h=180'} alt='https://tse3.mm.bing.net/th?id=OIP.cC-2qUAIWmvXAvxhUlnnLAHaH5&pid=Api&P=0&h=180'/>
       <p>{text}</p>
     </div>
   )
