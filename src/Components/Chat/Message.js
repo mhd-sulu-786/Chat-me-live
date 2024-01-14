ab9effa4cf96620cd00a7702c6dcfb280cb52791
Muhammad Sulaiman T <144138399+mhd-sulu-786@users.noreply.github.com>
2024-01-15 01:28:06 +0530

    Update Message.js

--- src/Components/Chat/Message.js
+++ src/Components/Chat/Message.js
@@ -10,8 +10,8 @@ const Message = ({message,user}) => {
   return (
     <div className={`messege-shower ${messegeClass}`}>
       <img src={messegeClass==="sent"?photourl:'https://tse3.mm.bing.net/th?id=OIP.cC-2qUAIWmvXAvxhUlnnLAHaH5&pid=Api&P=0&h=180'||profile} alt='https://tse3.mm.bing.net/th?id=OIP.cC-2qUAIWmvXAvxhUlnnLAHaH5&pid=Api&P=0&h=180'/>
-      <p>{text}</p><br/>
-    <span>{messageTime}</span>
+      <p>{text}<br/>
+    <span>{messageTime}</span></p>
       
     </div>
   )
